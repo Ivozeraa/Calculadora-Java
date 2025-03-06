7f017c2b6dae36f247f33ff449b7b21bda0d37a9
Ivozeraa <joseivo2008@gmail.com>
2025-03-06 16:37:47 -0300

    Frame Erik

--- src/App.java
+++ src/App.java
@@ -1,9 +1,28 @@
+
+import javax.swing.*;
+import java.awt.*;
+
+
 import classes.Calculadora;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Calculadora calculadora = new Calculadora();
 
-        calculadora.menu();
+        JFrame frame = new JFrame("Calculadora");
+        JLabel label = new JLabel("Opa");
+        JButton button = new JButton("Clique aqui");
+
+        frame.setVisible(true);
+        frame.setBounds(1, 1, 1920, 1080);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new FlowLayout());
+        button.setBackground(new Color(128, 0, 128));
+        button.addActionListener(e -> JOptionPane.showMessageDialog(frame, "Voce eh gado"));
+
+        frame.add(label);
+        frame.add(button);
+
+
+
     }
 }
